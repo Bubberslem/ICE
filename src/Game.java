319559d0bb8c61920b08efdd6c9567f3d791b8b9
Bubberslem <Rasmussenm0@gmail.com>
2024-12-10 12:27:41 +0100

    push

--- src/Game.java
+++ src/Game.java
@@ -11,63 +11,56 @@ public class Game {
 
     }
         public void menu() {
-        Scanner scanner = new Scanner(System.in);
         boolean running = true;
 
         while (running) {
-            ui.displayMsg("=== Welcome to Madseroth! ===");
-            System.out.println("1. New Game");
-            System.out.println("2. Load Game");
-            System.out.println("3. Exit Game");
-            System.out.println("Please choose an option adventurer!: ");
-
-            String Choice = scanner.nextLine();
-
+            ui.displayMsg("=== Welcome to "+ name+ "!"+" ===");
+            ui.displayMsg("1. New Game");
+            ui.displayMsg("2. Load Game");
+            ui.displayMsg("3. Exit Game");
+            String Choice = ui.promptText("Please choose an option adventurer!: ");
 
             switch (Choice) {
                 case "1":
-                    handleNewGame(scanner);
+                    handleNewGame();
                     break;
                 case "2":
                     System.out.println("Loading Game...");
                     break;
                     case "3":
-                        System.out.println("Exit Game...");
+                        System.out.println("Exiting Game...\nThanks for playing "+ name+"!");
+                        running = false;
                         break;
                         default:
                             System.out.println("Invalid Choice. Select an available option");
             }
-            System.out.println();
         }
-        scanner.close();
     }
-    private static void handleNewGame(Scanner scanner) {
+    private void handleNewGame() {
         while (true) {
-            System.out.println("=== Select a Save Slot===");
-            System.out.println("1. Save Slot 1");
-            System.out.println("2. Save Slot 2");
-            System.out.println("3. Save Slot 3");
-            System.out.println("4. Back to Main Menu");
-            System.out.println("Choose a Save Slot to continue!");
+            ui.displayMsg("=== Select a Save Slot===");
+            ui.displayMsg("1. Save Slot 1");
+            ui.displayMsg("2. Save Slot 2");
+            ui.displayMsg("3. Save Slot 3");
+            ui.displayMsg("4. Back to Main Menu");
+            String slotChoice = ui.promptText("Choose a Save Slot to continue!");
 
-            String slotChoice = scanner.nextLine();
             switch (slotChoice) {
                 case "1":
-                    System.out.println("Save Slot 1 selected... starting New Game");
+                    ui.displayMsg("Save Slot 1 selected... starting New Game");
                     return;
                     case "2":
-                        System.out.println("Save Slot 2 selected... starting New Game");
+                        ui.displayMsg("Save Slot 2 selected... starting New Game");
                         return;
                         case "3":
-                            System.out.println("Save Slot 3 selected... starting New Game");
+                            ui.displayMsg("Save Slot 3 selected... starting New Game");
                             return;
                             case "4":
-                                System.out.println("Returning to Main Menu...");
+                                ui.displayMsg("Returning to Main Menu...");
                                 return;
                                 default:
-                                    System.out.println("Invalid Choice. Select an available option");
+                                    ui.displayMsg("Invalid Choice. Select an available option");
             }
-            System.out.println();
         }
     }
 }
